bdbfdda6e6f924d13aad69b212dc450c2cb6315f
Md. Nahidul Islam <nahidshrabon@gmail.com>
2023-04-20 23:36:42 +0600

    Initial Commit

---
+++ simple-chat/simple_chatApp.swift
@@ -0,0 +1,17 @@
+//
+//  simple_chatApp.swift
+//  simple-chat
+//
+//  Created by Md. Nahidul Islam on 20/4/23.
+//
+
+import SwiftUI
+
+@main
+struct simple_chatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
